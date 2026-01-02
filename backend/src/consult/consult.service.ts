704442bb1b7e262fd780897fb5281aa2ab8d5d96
kluvnoa <okp.great.51142111@gmail.com>
2026-01-02 17:55:25 +0900

    Resolve CI check errors

--- backend/src/consult/consult.service.ts
+++ backend/src/consult/consult.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service.js';
+import { PrismaService } from '../prisma/prisma.service';
 import { ConsultHistory } from '@prisma/client';
 
 interface AdviceRequest {
